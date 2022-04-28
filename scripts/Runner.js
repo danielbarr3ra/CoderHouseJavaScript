b34448f569585078513c4b760b8c3f6d2e415f1a
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-04-28 17:44:07 -0500

    added formating to the webpage

--- scripts/Runner.js
+++
@@ -1,9 +0,0 @@
-class Runner {
-    constructor(name, gender, age) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-        this.maxHR = 208 - 0.7 * this.age;
-        this.races = [];
-    }
-}
