307b92cdcb23cff700a65328bff187c734d90e71
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-04-27 21:39:35 -0500

    fixed structure, added event handler to add racer

---
+++ script.js
@@ -0,0 +1,7 @@
+const btnAddUser = document.querySelector("button");
+
+btnAddUser.addEventListener("clcik", addRacer);
+
+function addRacer() {
+    console.log("beep boop adding user");
+}
---
+++ scripts/LeaderBoard.js
@@ -0,0 +1,10 @@
+class LeaderBoard {
+    constructor() {
+        this.members = []; //an empty array of members we will add them in the future
+    }
+    addMember(Race)
+
+    sortByDistance();
+    sortByBestPace();
+    sortByVo2Mac();
+}
