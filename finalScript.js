3e34ab56e042f6e51087e3a16ae16725621b5d11
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-05-28 13:06:15 -0500

    method that reads form data into json

--- finalScript.js
+++ finalScript.js
@@ -4,17 +4,38 @@ $(document).ready(function () {
     // buttons 
     const showLeaderBoardBtn = $("#showLeaderboard");
     const addRacerBtn = $("#addRacer");
+    const submitRacerFormBtn = $("#submitRacerForm");
+
+    //forms
+    const addRacerForm = $("#addRacerForm")
+
+    //helper functionss 
+    //using arrow functions because work also usses them
+    const formToJson = (aForm) => {
+        const arrayAnswers = $(aForm).serializeArray()
+        const jsonObject = {}
+        //jquery for each eleemnt
+        $.each(arrayAnswers, function () {
+            jsonObject[this.name] = this.value || "";
+        });
+        return jsonObject
+    }
     //click handlers 
     showLeaderBoardBtn.click(function () {
         $("#leaderBoardWrapper").toggle();
     });
+
     addRacerBtn.click(function () {
         $("#racerFormWrapper").toggle();
     })
+    // submit racerFrom
+
+    addRacerForm.on("submit", function (e) {
+        e.preventDefault();
+        const form = $(e.target) //this gets the form since the event has the target, I think?
+        const racerJson = formToJson(form)
+        console.log(racerJson)
+        alert("new method")
+        alert(racerJson)
+    })
 });
-
-
-const submitRacerForm = () => {
-    document.getElementById("addRacerForm").style.display = 'none'
-    alert("submitted")
-}
