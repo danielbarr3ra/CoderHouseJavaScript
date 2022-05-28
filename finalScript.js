7483bf67a3907587aff9094968ec8f26dcedeafd
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-05-28 10:50:03 -0500

    using jquery to display the leaderboard

--- finalScript.js
+++ finalScript.js
@@ -1,7 +1,6 @@
 // constant buuttons in the page that will trigger 
 const addRacerBtn = document.getElementById("addRacer");
 const submitRacerBtn = document.getElementById("submitRacerForm")
-
 // functions
 const showSubmitRacerForm = () => {
     document.getElementById("addRacerForm").style.display = 'flex'
@@ -14,3 +13,14 @@ const submitRacerForm = () => {
 //event listeners
 addRacerBtn.addEventListener("click", showSubmitRacerForm);
 submitRacerBtn.addEventListener("click", submitRacerForm);
+
+// leaderboard but unshow methods
+
+$(document).ready(function () {
+    // jquery has been loaded 
+    const showLeaderBoardBtn = $("#showLeaderboard");
+    //function 
+    showLeaderBoardBtn.click(function () {
+        $("#globalBoard").toggle();
+    });
+});
