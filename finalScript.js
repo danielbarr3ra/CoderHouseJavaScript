3a9e59c889b6b1d7bc77aa9097ef914e6757fc6f
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-05-29 23:10:13 -0500

    localStorage now saving the racers

--- finalScript.js
+++ finalScript.js
@@ -26,11 +26,7 @@ class Utilities {
 }
 
 $(document).ready(function () {
-    // helpufl html for cards
-    // var racerRowLeaderBoard = '<div class = "racerRow">';
-
-    // storage
-    const globalBoard = new LeaderBoard();
+    let globalBoard = localStorage.getItem('globalBoard') !== null ? JSON.parse(localStorage.getItem('globalBoard')) : new LeaderBoard();
 
     // buttons 
     const showLeaderBoardBtn = $("#showLeaderboard");
@@ -62,6 +58,7 @@ $(document).ready(function () {
         newRacer = collectRacerForm(answers);
         globalBoard.addRacer(newRacer);
         $("#racerFormWrapper").toggle();
+        localStorage.setItem("globalBoard", JSON.stringify(globalBoard));
     })
 
 
@@ -84,62 +81,4 @@ $(document).ready(function () {
         $("#leaderBoardPlane").append(HTML);
 
     }
-    // //helper functions, using arrow functions because work also usses them
-
-    // //update leaderboard
-    // const updateCurrentLeaderBoard = () => {
-    //     $("#leaderBoardPlane").empty()
-    //     let HTML = ""
-    //     RacersList.forEach(racer => {
-    //         let { racersName, racersAge, racersGender } = racer
-    //         HTML += `<div class="leaderBoardRow"><div class="leaderBoardRow-number">${racersGender}</div><div class="leaderBoardRow-name">${racersName}</div><div class="leaderBoardRow-age">${racersAge}</div></div>`
-    //     });
-    //     $("#leaderBoardPlane").append(HTML);
-
-    //     //add hmtl to object 
-    // }
-
-    // //convert form to json
-    // const formToJson = (aForm) => {
-    //     const arrayAnswers = $(aForm).serializeArray()
-    //     const jsonObject = {}
-    //     //jquery for each eleemnt
-    //     $.each(arrayAnswers, function () {
-    //         jsonObject[this.name] = this.value || "";
-    //     });
-    //     return jsonObject
-    // }
-
-    // //submit runner object to database
-    // const postRacerToList = (aRacer) => {
-    //     RacersList.push(aRacer);
-    // }
-
-    // //might neeed later
-    // const getRacerFromList = (aRacersName) => {
-
-    // }
-
-    // //click handlers 
-    // showLeaderBoardBtn.click(function () {
-    //     $("#leaderBoardWrapper").toggle();
-    // });
-
-    // addRacerBtn.click(function () {
-    //     $("#racerFormWrapper").toggle();
-
-    // })
-
-    // // submit form handlers
-    // addRacerForm.on("submit", function (e) {
-    //     e.preventDefault();
-    //     const form = $(e.target)
-    //     const racerJson = formToJson(form)
-    //     postRacerToList(racerJson);
-    //     $("#racerFormWrapper").toggle();
-    //     $("#competitionWrapper").toggle();
-
-    //     //updateCurrentLeaderBoard();
-
-    // })
 });
